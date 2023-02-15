17f2bf15e404dec83c8f7c528c6cf5e15b5aaa6f
Talha Bin Alam <talhabinalam98@gmail.com>
2023-02-15 14:18:48 +0600

    Added message alert

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask_mail import Mail
@@ -197,6 +197,7 @@ def contact():
             recipients=[parameters["gmail-user"]],
             body=message + "\n" + phone,
         )
+        flash("Thanks for submitting your details. We will get back to you soon", "success")
     return render_template("contact.html", parameters=parameters)
 
 
